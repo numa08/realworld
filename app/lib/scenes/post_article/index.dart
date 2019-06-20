15fbba5010d0a665bec88b6f852497b09b938a1f
numa08 <n511287@gmail.com>
2019-06-20 23:23:05 +0900

    todo layout post screen

--- app/lib/scenes/post_article/index.dart
+++ app/lib/scenes/post_article/index.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:app/bloc/bloc.dart';
 import 'package:app/repositories/repositories.dart';
 import 'package:bloc_provider/bloc_provider.dart';
@@ -17,13 +19,112 @@ class PostScene extends StatelessWidget {
           return Scaffold(
             appBar: AppBar(
               title: Text('conduit'),
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () => bloc.postArticle.add(null),
+                )
+              ],
             ),
-            body: Container(),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () => bloc.postArticle.add(null),
-              child: Icon(Icons.add),
+            body: _Home(
+              initialTitle: Stream.empty(),
+              initialDescription: Stream.empty(),
+              initialBody: Stream.empty(),
+              initialTag: Stream.empty(),
+              titleError: Stream.empty(),
+              descriptionError: Stream.empty(),
+              bodyError: Stream.empty(),
             ),
           );
         }),
+      );
+}
+
+class _Home extends StatefulWidget {
+  final Stream<String> initialTitle;
+  final Stream<String> initialDescription;
+  final Stream<String> initialBody;
+  final Stream<String> initialTag;
+  final Stream<String> titleError;
+  final Stream<String> descriptionError;
+  final Stream<String> bodyError;
+
+  const _Home(
+      {Key key,
+      this.initialTitle,
+      this.initialDescription,
+      this.initialBody,
+      this.initialTag,
+      this.titleError,
+      this.descriptionError,
+      this.bodyError})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends State<_Home> {
+  String initialTitle;
+  String initialDescription;
+  String initialBody;
+  String initialTag;
+  String titleError;
+  String descriptionError;
+  String bodyError;
+
+  final List<StreamSubscription> subscriptions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    subscriptions.addAll([
+      widget.initialTitle.listen((t) {
+        setState(() {
+          this.initialTitle = t;
+        });
+      }),
+      widget.initialDescription.listen((t) {
+        setState(() {
+          this.initialDescription = t;
+        });
+      }),
+      widget.initialBody.listen((t) {
+        setState(() {
+          this.initialBody = t;
+        });
+      }),
+      widget.initialTag.listen((t) {
+        setState(() {
+          this.initialTag = t;
+        });
+      }),
+      widget.titleError.listen((e) {
+        setState(() {
+          this.titleError = e;
+        });
+      }),
+      widget.descriptionError.listen((e) {
+        setState(() {
+          this.descriptionError = e;
+        });
+      }),
+      widget.bodyError.listen((e) {
+        setState(() {
+          this.bodyError = e;
+        });
+      })
+    ]);
+  }
+
+  @override
+  void dispose() {
+    subscriptions.forEach((s) => s.cancel());
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+        child: Column(),
       );
 }
